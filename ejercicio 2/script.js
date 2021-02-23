37b5811bd13d1a9c7bd75cc8ad6dd873ca9a2761
marianosaban <marianosaban@gmail.com>
2021-02-23 15:34:36 -0300

    Ordenando

---
+++ ejercicio 2/script.js
@@ -0,0 +1,9 @@
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+for(var i=0; i<7; i++) {
+    if (i % 2 === 1) {
+        console.log(dias[i] + " Es un día par");
+    }
+    else if(i===6(
+        alert: "¡Vamooo es Domingo!"
+    )
+}
