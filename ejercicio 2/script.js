faef23284b9f0c8897e977542604974a349a2796
marianosaban <marianosaban@gmail.com>
2021-02-23 15:38:54 -0300

    Commit ejercicio 2

--- ejercicio 2/script.js
+++ ejercicio 2/script.js
@@ -3,7 +3,7 @@ for(var i=0; i<7; i++) {
     if (i % 2 === 1) {
         console.log(dias[i] + " Es un día par");
     }
-    else if(i===6(
-        alert: "¡Vamooo es Domingo!"
-    )
+    else if(i == 6){
+        alert ("¡Vamooo! ¡Es Domingo!")
+    }   
 }
