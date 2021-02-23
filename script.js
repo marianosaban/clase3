f0cc109298ad673035b13b797a696a5c9b99616f
marianosaban <marianosaban@gmail.com>
2021-02-23 15:19:07 -0300

    ejercicio 1

---
+++ script.js
@@ -0,0 +1,9 @@
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+for(var i=0; i<7; i++) {
+    if (i % 2 === 1) {
+        console.log(dias[i] + " Es un día par");
+    }
+    if(i == 6){
+        alert("A disfrutar que es domingo");
+    }
+}
